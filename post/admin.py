659274c6677862ba0f9e1d48e412345732d97aa7
kyrgyz-dev <okurman.kenzhegulov@gmail.com>
2025-08-22 19:30:09 +0600

    Post models and admin

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from post.models import Post, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'category', 'created_at']
+    list_display_links = ['id', 'title', 'author', 'category']
+    list_filter = ['category', 'author']
+    search_fields = ['title', 'content']
--- post/models.py
+++ post/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    content = models.TextField(verbose_name="Содержание")
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор",
+        related_name='posts',
+    )
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        verbose_name='Категория',
+        related_name='posts',
+    )
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+    def __str__(self):
+        return self.title
